b0b0ce70923ca3e5b1d49cb772657f81c5e2b5d2
valerio <v.scisci@t33.it>
2020-05-07 10:27:23 +0200

    Aggiunto script che valorizza gli attributi delle carte. In seguito verranno aggiunte le Sprites

--- Assets/GeneratoreCarte.cs
+++ Assets/GeneratoreCarte.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Classe che si occupa di generare il mazzo di carte e di assegnare una posizione sulla griglia a tutte le carte
+
 public class GeneratoreCarte : MonoBehaviour
 {
-    public GameObject refCarta; // Referenza Carta
-    private GameObject[,] mazzoCarte = new GameObject[4,4];
-    
-    // Start is called before the first frame update
-    void Start()
+    private GameObject refCarta; // Referenza Carta
+    private GameObject[,] mazzoCarte = new GameObject[4,4]; // Crea il mazzo di carte vuoto
+
+    private void Awake()
     {
-        refCarta = GameObject.FindGameObjectWithTag("Carta"); // Prende l'oggetto con il tag carta
+        refCarta = GameObject.FindGameObjectWithTag("Carta"); // Prende l'oggetto con il tag Carta
 
         // Cicliamo 16 volte per generare le carte e assegnargli la posizione
         for (int i = 0; i < 4; i++)
@@ -20,7 +21,13 @@ public class GeneratoreCarte : MonoBehaviour
                 mazzoCarte[i, j] = GeneraCarta(i, j);
             }
         }
-        refCarta.SetActive(false);
+        refCarta.SetActive(false); // Disattiva dal gioco la nostra carta "Blueprint" che ci permette di creare le altre
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
     }
 
     // Update is called once per frame
@@ -29,13 +36,25 @@ public class GeneratoreCarte : MonoBehaviour
         
     }
 
-    //
-    GameObject GeneraCarta(int i, int j)
+    // Metodo che instanzai una carta e le assegna una posizione
+    private GameObject GeneraCarta(int i, int j)
     {
         GameObject cloneCarta = Instantiate(refCarta); // Creiamo un clone della carta
         cloneCarta.SetActive(true); // Attiviamo la carta
         cloneCarta.transform.position = new Vector2(i * 3 - 4, j * 4 - 6);
 
         return cloneCarta;
+    }
+
+    // Setter Mazzo Carte
+    public void setMazzoCarte(GameObject[,] mazzo)
+    {
+        this.mazzoCarte = mazzo;
+    }
+
+    // Getter Mazzo Carte
+    public GameObject[,] getMazzoCarte()
+    {
+        return this.mazzoCarte;
     }
 }
--- Assets/scriptCarta.cs
+++ Assets/scriptCarta.cs
@@ -2,26 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scriptCarta : MonoBehaviour
+// Classe che definisce tutti i comportamenti e le informazioni della singola carta
+public class ScriptCarta : MonoBehaviour
 {
-    public int numeroCarta;
-    public string segnoCarta;
-    public Sprite immagineCarta;
+    private int numeroCarta;
+    private string semeCarta;
+    private Sprite immagineCarta;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
+        Debug.Log(this.numeroCarta);
+        Debug.Log(this.semeCarta);
+        Debug.Log(this.immagineCarta);
+        GetComponent<SpriteRenderer>().sprite = this.immagineCarta;
+    }
+
+    // Setter numeroCarta
+    public void setNumeroCarta(int numeroCarta)
+    {
+        this.numeroCarta = numeroCarta;
+    }
+
+    // Setter segnoCarta
+    public void setSemeCarta(string semeCarta)
+    {
+        this.semeCarta = semeCarta;
+    }
+
+    // Setter immagineCarta
+    public void setImmagineCarta(Sprite immagineCarta)
+    {
+        this.immagineCarta = immagineCarta;
     }
 }
