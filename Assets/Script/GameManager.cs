583f2167eb137ea5a997ad20156e2ddc8990fb1f
valerio <v.scisci@t33.it>
2020-05-09 10:18:48 +0200

    Aggiunto il Game Manager, che per ora si occupa di verificare le carte cliccate e di distruggerle se uguali.

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Classe che gestisce il gioco nella sua interezza
+public class GameManager : MonoBehaviour
+{
+    private GeneratoreCarte refGeneratore;
+    private GameObject cartaCliccata_1, cartaCliccata_2; // Variabili per memorizzare le carte che sono state cliccate
+    private int contatoreClick = 2; // Contatore che memorizza il numero di click disponibili
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        refGeneratore = GameObject.FindGameObjectWithTag("GeneratoreCarte").GetComponent<GeneratoreCarte>(); // Prendiamo la referenza del generatore di carte
+        GameObject[,] mazzoCarte = refGeneratore.getMazzoCarte(); // Recuperiamo il mazzo di carte generato dal generatore
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Metodo per sottrarre un click disponibile e verificare se il contatore è a 0 per poi lanciare il metodo che verifica se le carte cliccate sono uguali
+    public void SottraiClick(GameObject cartaCliccata)
+    {
+        contatoreClick -= 1;
+        if (contatoreClick == 1) {
+            cartaCliccata_1 = cartaCliccata;
+        } else {
+            cartaCliccata_2 = cartaCliccata;
+            CheckNomi();
+        }
+    }
+
+    // Metodo che controlla se le carte cliccate sono identiche per poi andarle a distruggere
+    private void CheckNomi()
+    {
+        if (cartaCliccata_1.GetComponent<SpriteRenderer>().sprite.name.Equals(cartaCliccata_2.GetComponent<SpriteRenderer>().sprite.name))
+        { // Se sono uguali disattivale
+            cartaCliccata_1.SetActive(false);
+            cartaCliccata_2.SetActive(false);
+        } else { // Altrimenti reset carte 
+            cartaCliccata_1.GetComponent<ScriptCarta>().setCliccata();
+            cartaCliccata_2.GetComponent<ScriptCarta>().setCliccata();
+        }
+        contatoreClick = 2; // Reset contatore
+    }
+}
--- Assets/Script/ScriptCarta.cs
+++ Assets/Script/ScriptCarta.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 // Classe che definisce tutti i comportamenti e le informazioni della singola carta
 public class ScriptCarta : MonoBehaviour
 {
+    private bool cliccata = false;
+    private GameManager refGameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        refGameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>(); // Prendiamo la referenza del game manager
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
 
+    // Metodo che memorizza il fatto che la carta è stata cliccata, la disattiva e diminuisce di uno il counter del Game Manager
+    private void OnMouseDown()
+    {
+        cliccata = true;
+        GetComponent<BoxCollider2D>().enabled = false;
+        refGameManager.SottraiClick(gameObject);
+    }
+
+    // Setter cliccata
+    public void setCliccata()
+    {
+        cliccata = false;
+        GetComponent<BoxCollider2D>().enabled = true;
     }
 
     // Setter immagineCarta
